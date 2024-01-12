241f47fec44fd260ef2160fc7c065f5fb569e662
Suraj Khayaamali <surajkhayamali.sk@gmail.com>
2024-01-12 10:47:12 +0545

    update: game entity

---
+++ backend/src/entities/game.entity.ts
@@ -0,0 +1,31 @@
+import { Column, Entity, ManyToOne } from 'typeorm';
+import { BaseEntity } from './base.entity';
+import { User } from './user.entity';
+import { GameMode } from '../enums/gameMode.enum';
+
+@Entity()
+export class Game extends BaseEntity {
+  @Column()
+  slug: string;
+
+  @ManyToOne(() => User, { nullable: true })
+  whitePlayer?: User;
+
+  @ManyToOne(() => User, { nullable: true })
+  blackPlayer?: User;
+
+  @Column()
+  gameMode: GameMode = GameMode.PLAYER_VS_PLAYER;
+
+  @Column({ nullable: true })
+  timeLimit?: number; // in seconds
+
+  @Column()
+  initialBoardState: string; // FEN notation
+
+  @Column()
+  isGameOver: boolean = false;
+
+  @Column({ nullable: true })
+  hasWhitePlayerWon: boolean;
+}
---
+++ backend/src/enums/gameMode.enum.ts
@@ -0,0 +1,4 @@
+export enum GameMode {
+  PLAYER_VS_PLAYER = 'PvP',
+  PLAYER_VS_COMPUTER = 'PvC',
+}
