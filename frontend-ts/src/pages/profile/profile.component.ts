452dc52da40221e13c210e61179be5de93ae4997
Suraj Khayamali <surajkhayamali.sk@gmail.com>
2024-01-13 09:27:32 +0545

    feat: enable and disable profile edit form

--- frontend-ts/src/pages/profile/profile.component.ts
+++ frontend-ts/src/pages/profile/profile.component.ts
@@ -3,32 +3,34 @@ export const component = /* html */ `
   <h1 class="text-3xl font-bold">User settings</h1>
 
   <form id="generalInfoForm" class="bg-neutral p-8 rounded-lg">
-    <h2 class="text-xl font-bold mb-4">General Information</h2>
+    <fieldset disabled id="generalInfoFieldset" class="p-0">
+      <h2 class="text-xl font-bold mb-4">General Information</h2>
 
-    <div class="form-control">
-      <label for="firstName" class="label">First Name</label>
-      <input type="text" name="firstName" id="firstName" placeholder="First Name" class="input input-bordered" />
-    </div>
+      <div class="form-control">
+        <label for="firstName" class="label">First Name</label>
+        <input type="text" name="firstName" id="firstName" placeholder="First Name" class="input input-bordered" />
+      </div>
 
-    <div class="form-control">
-      <label for="middleName" class="label">Middle Name</label>
-      <input type="text" name="middleName" id="middleName" placeholder="Middle Name" class="input input-bordered" />
-    </div>
+      <div class="form-control">
+        <label for="middleName" class="label">Middle Name</label>
+        <input type="text" name="middleName" id="middleName" placeholder="Middle Name" class="input input-bordered" />
+      </div>
 
-    <div class="form-control">
-      <label for="lastName" class="label">Last Name</label>
-      <input type="text" name="lastName" id="lastName" placeholder="Last Name" class="input input-bordered" />
-    </div>
+      <div class="form-control">
+        <label for="lastName" class="label">Last Name</label>
+        <input type="text" name="lastName" id="lastName" placeholder="Last Name" class="input input-bordered" />
+      </div>
 
-    <div class="form-control">
-      <label for="email" class="label">Email</label>
-      <input type="email" name="email" id="email" placeholder="Email" class="input input-bordered" />
-    </div>
+      <div class="form-control">
+        <label for="email" class="label">Email</label>
+        <input type="email" name="email" id="email" placeholder="Email" class="input input-bordered" />
+      </div>
 
-    <div class="form-control">
-      <label for="username" class="label">Username</label>
-      <input type="text" name="username" id="username" placeholder="Username" class="input input-bordered" />
-    </div>
+      <div class="form-control">
+        <label for="username" class="label">Username</label>
+        <input type="text" name="username" id="username" placeholder="Username" class="input input-bordered" />
+      </div>
+    </fieldset>
 
     <button id="editInfoBtn" class="btn btn-primary btn-sm mt-4">Edit</button>
     <button type="submit" id="updateInfoBtn" class="btn btn-primary btn-sm mt-4 hidden">Update</button>
@@ -36,31 +38,33 @@ export const component = /* html */ `
   </form>
 
   <form id="passwordForm" class="bg-neutral p-8 rounded-lg">
-    <h2 class="text-xl font-bold mt-4">Password Information</h2>
+    <fieldset disabled id="passwordFieldset" class="p-0">
+      <h2 class="text-xl font-bold mt-4">Password Information</h2>
 
-    <div class="form-control">
-      <label for="oldPassword" class="label">Old Password</label>
-      <div class="join">
-        <input type="password" name="oldPassword" id="oldPassword" placeholder="Old Password" class="input input-bordered join-item w-full" />
-        <button id="oldPasswordVissiblityToogleBtn" class="btn join-item rounded-r-full"><i class="fa-solid fa-eye"></i></button>
+      <div class="form-control">
+        <label for="oldPassword" class="label">Old Password</label>
+        <div class="join">
+          <input type="password" name="oldPassword" id="oldPassword" placeholder="Old Password" class="input input-bordered join-item w-full" />
+          <button id="oldPasswordVissiblityToogleBtn" class="btn join-item rounded-r-full"><i class="fa-solid fa-eye"></i></button>
+        </div>
       </div>
-    </div>
 
-    <div class="form-control">
-      <label for="newPassword" class="label">New Password</label>
-      <div class="join">
-        <input type="password" name="newPassword" id="newPassword" placeholder="New Password" class="input input-bordered join-item w-full" />
-        <button id="newPasswordVissiblityToogleBtn" class="btn join-item rounded-r-full"><i class="fa-solid fa-eye"></i></button>
+      <div class="form-control">
+        <label for="newPassword" class="label">New Password</label>
+        <div class="join">
+          <input type="password" name="newPassword" id="newPassword" placeholder="New Password" class="input input-bordered join-item w-full" />
+          <button id="newPasswordVissiblityToogleBtn" class="btn join-item rounded-r-full"><i class="fa-solid fa-eye"></i></button>
+        </div>
       </div>
-    </div>
 
-    <div class="form-control">
-      <label for="confirmPassword" class="label">Confirm Password</label>
-      <div class="join">
-        <input type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirm Password" class="input input-bordered join-item w-full" />
-        <button id="confirmPasswordVissiblityToogleBtn" class="btn join-item rounded-r-full"><i class="fa-solid fa-eye"></i></button>
+      <div class="form-control">
+        <label for="confirmPassword" class="label">Confirm Password</label>
+        <div class="join">
+          <input type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirm Password" class="input input-bordered join-item w-full" />
+          <button id="confirmPasswordVissiblityToogleBtn" class="btn join-item rounded-r-full"><i class="fa-solid fa-eye"></i></button>
+        </div>
       </div>
-    </div>
+    </fieldset>
     
     <button id="editPasswordBtn" class="btn btn-primary btn-sm mt-4">Edit</button>
     <button type="submit" id="updatePasswordBtn" class="btn btn-primary btn-sm mt-4 hidden">Update</button>
@@ -123,6 +127,9 @@ export const afterInitialize = () => {
   const generalInfoForm = document.getElementById(
     'generalInfoForm'
   ) as HTMLFormElement;
+  const generalInfoFieldset = document.getElementById(
+    'generalInfoFieldset'
+  ) as HTMLFieldSetElement;
   const editInfoBtn = document.getElementById(
     'editInfoBtn'
   ) as HTMLButtonElement;
@@ -136,6 +143,7 @@ export const afterInitialize = () => {
   editInfoBtn.addEventListener('click', (e) => {
     e.preventDefault();
 
+    generalInfoFieldset.removeAttribute('disabled');
     editInfoBtn.classList.add('hidden');
     updateInfoBtn.classList.remove('hidden');
     cancelEditInfoBtn.classList.remove('hidden');
@@ -146,6 +154,7 @@ export const afterInitialize = () => {
 
     generalInfoForm.reset();
 
+    generalInfoFieldset.setAttribute('disabled', '');
     editInfoBtn.classList.remove('hidden');
     updateInfoBtn.classList.add('hidden');
     cancelEditInfoBtn.classList.add('hidden');
@@ -155,6 +164,9 @@ export const afterInitialize = () => {
   const passwordForm = document.getElementById(
     'passwordForm'
   ) as HTMLFormElement;
+  const passwordFieldset = document.getElementById(
+    'passwordFieldset'
+  ) as HTMLFieldSetElement;
   const editPasswordBtn = document.getElementById(
     'editPasswordBtn'
   ) as HTMLButtonElement;
@@ -168,6 +180,7 @@ export const afterInitialize = () => {
   editPasswordBtn.addEventListener('click', (e) => {
     e.preventDefault();
 
+    passwordFieldset.removeAttribute('disabled');
     editPasswordBtn.classList.add('hidden');
     updatePasswordBtn.classList.remove('hidden');
     cancelEditPasswordBtn.classList.remove('hidden');
@@ -178,6 +191,7 @@ export const afterInitialize = () => {
 
     passwordForm.reset();
 
+    passwordFieldset.setAttribute('disabled', '');
     editPasswordBtn.classList.remove('hidden');
     updatePasswordBtn.classList.add('hidden');
     cancelEditPasswordBtn.classList.add('hidden');
