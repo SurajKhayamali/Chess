50d77923b7a031ec27f9f48472c23c5781948c23
Suraj Khayamali <surajkhayamali.sk@gmail.com>
2024-01-13 14:51:12 +0545

    update: adding validation at register component

--- frontend-ts/src/pages/auth/register/register.component.ts
+++ frontend-ts/src/pages/auth/register/register.component.ts
@@ -3,10 +3,12 @@ import { NavigationMode } from 'enums/route.enum';
 import { createScriptTag } from 'helpers/createScriptTag.helper';
 import { ToastType, displayToast } from 'helpers/toast.helper';
 import { SignupDto } from 'interfaces/auth.interface';
+import { signupSchema } from 'schemas/auth.schema';
 import { handleNavigation } from 'scripts/router';
 import { handleRegister } from 'services/auth.service';
+import { AnyObjectSchema, ValidationError } from 'yup';
 
-export const component = `
+export const component = /* html */ `
     <div class="container w-full h-full flex justify-center items-center">
         <div class="p-12 rounded-lg">
             <h1 class="text-3xl font-bold mb-8">Register</h1>
@@ -14,32 +16,38 @@ export const component = `
                 <label class="block">
                     <span>First name*</span>
                     <input type="text" class="input input-bordered w-full max-w-xs" name="firstName"
-                        placeholder="Enter your first name" required />
+                        placeholder="Enter your first name" />
+                    <p class="text text-error max-w-xs"></p>
                 </label>
                 <label class="block">
                     <span>Middle name</span>
                     <input type="text" class="input input-bordered w-full max-w-xs" name="middleName"
                         placeholder="Enter your middle name" />
+                    <p class="text text-error max-w-xs"></p>
                 </label>
                 <label class="block">
                     <span>Last name*</span>
                     <input type="text" class="input input-bordered w-full max-w-xs" name="lastName"
-                        placeholder="Enter your last name" required />
+                        placeholder="Enter your last name" />
+                    <p class="text text-error max-w-xs"></p>
                 </label>
                 <label class="block">
                     <span>Email address</span>
                     <input type="email" class="input input-bordered w-full max-w-xs" name="email"
-                        placeholder="john@example.com" required />
+                        placeholder="john@example.com" />
+                    <p class="text text-error max-w-xs"></p>
                 </label>
                 <label class="block">
                     <span>Username*</span>
                     <input type="text" class="input input-bordered w-full max-w-xs" name="username"
-                        placeholder="Enter your user name" required />
+                        placeholder="Enter your user name" />
+                    <p class="text text-error max-w-xs"></p>
                 </label>
                 <label class="block">
                     <span>Password*</span>
                     <input type="password" class="input input-bordered w-full max-w-xs" name="password"
-                        placeholder="Enter your password" autocomplete="on" required />
+                        placeholder="Enter your password" autocomplete="on" />
+                    <p class="text text-error max-w-xs"></p>
                 </label>
 
                 <div class="mt-2">
@@ -63,8 +71,56 @@ export const loadScripts = () => {
   return createScriptTag('/auth/register/script.ts', 'module');
 };
 
+const validateForm = (form: HTMLFormElement, schema: AnyObjectSchema) => {
+  const formData = new FormData(form);
+  const body = Object.fromEntries(formData.entries());
+
+  try {
+    schema.validateSync(body, {
+      abortEarly: false,
+    });
+    return true;
+  } catch (error) {
+    if (error instanceof ValidationError) {
+      const errors = error.inner.reduce((acc, curr) => {
+        acc[curr.path as string] = curr.message;
+        return acc;
+      }, {} as Record<string, string>);
+      console.log('errors: ', errors);
+
+      for (const [key, value] of Object.entries(errors)) {
+        if (!value) continue;
+        const input = form.querySelector(`[name="${key}"]`)!;
+        if (!input) continue;
+
+        input.classList.add('input-error');
+
+        const errorMessage = input.parentElement?.querySelector(
+          '.text-error'
+        ) as HTMLParagraphElement;
+        if (errorMessage) errorMessage.innerText = value;
+      }
+    }
+    return false;
+  }
+};
+
+const clearErrorsOnChange = (form: HTMLFormElement) => {
+  const inputs = form.querySelectorAll('input')!;
+  inputs.forEach((input) => {
+    input.addEventListener('input', () => {
+      input.classList.remove('input-error');
+      const errorMessage = input.parentElement?.querySelector(
+        '.text-error'
+      ) as HTMLParagraphElement;
+      if (errorMessage) errorMessage.innerText = '';
+    });
+  });
+};
+
 export const afterInitialize = () => {
   const form = document.querySelector('form')!;
+  clearErrorsOnChange(form);
 
   form.addEventListener('submit', async (e) => {
     e.preventDefault();
@@ -72,6 +128,9 @@ export const afterInitialize = () => {
 
     const body = Object.fromEntries(formData.entries());
     // console.log('Body: ', body);
+    const isValid = validateForm(form, signupSchema);
+    // console.log('isValid: ', isValid);
+    if (!isValid) return;
 
     if (!body.middleName) delete body.middleName;
     delete body.agree;
--- frontend-ts/src/schemas/auth.schema.ts
+++ frontend-ts/src/schemas/auth.schema.ts
@@ -1,5 +1,56 @@
 import * as yup from 'yup';
 
+export const signupSchema = yup.object().shape({
+  firstName: yup
+    .string()
+    .min(3, 'First name must be at least 3 characters long')
+    .max(30, 'First name must be at most 30 characters long')
+    .required('First name is required'),
+  middleName: yup.string().max(30, 'Middle name must be at most 30 characters'),
+  lastName: yup
+    .string()
+    .min(3, 'Last name must be at least 3 characters long')
+    .max(30, 'Last name must be at most 30 characters long')
+    .required('Last name is required'),
+  username: yup
+    .string()
+    .min(3, 'Username must be at least 3 characters')
+    .max(30, 'Username must be at most 30 characters')
+    .matches(
+      /^[a-zA-Z0-9_]*$/,
+      'Username must only contain letters, numbers, and underscores'
+    )
+    .required('Username is required'),
+  email: yup
+    .string()
+    .email('Email must be a valid email')
+    .required('Email is required'),
+  password: yup
+    .string()
+    .min(8, 'Password must be at least 8 characters')
+    .required('Password is required'),
+});
+
+export const loginSchema = yup.object().shape({
+  username: yup
+    .string()
+    .min(3, 'Username must be at least 3 characters')
+    .max(30, 'Username must be at most 30 characters')
+    .matches(
+      /^[a-zA-Z0-9_]*$/,
+      'Username must only contain letters, numbers, and underscores'
+    ),
+  email: yup.string().email('Email must be a valid email'),
+  emailOrUsername: yup
+    .string()
+    .oneOf(['email', 'username'])
+    .required('Email or username is required'),
+  password: yup
+    .string()
+    .min(8, 'Password must be at least 8 characters')
+    .required('Password is required'),
+});
+
 export const changePasswordSchema = yup.object().shape({
   oldPassword: yup
     .string()
