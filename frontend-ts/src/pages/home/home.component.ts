ac0c4f15e172568f740a45464f5cf015215b94be
Suraj Khayamali <surajkhayamali.sk@gmail.com>
2024-01-14 10:49:35 +0545

    update: displaying pre-game start mode options in home page

--- frontend-ts/src/pages/home/home.component.ts
+++ frontend-ts/src/pages/home/home.component.ts
@@ -2,12 +2,49 @@
 // import { reInitializeMessage } from 'scripts/message';
 // import { generateBoardWithFENString } from 'scripts/parseFEN';
 
-import { renderChatContainer } from 'components/chat/chatContainer.component';
-import { SocketEvent } from 'enums/socket.enum';
+// import { renderChatContainer } from 'components/chat/chatContainer.component';
+// import { SocketEvent } from 'enums/socket.enum';
 
 export const component = /*html*/ `
   <div class="container">
-    <div id="home-chat-container"></div>
+    <div class="grid grid-cols-3 gap-4">
+      <div class="card card-compact bg-base-300 transition-colors hover:bg-primary">
+        <div class="card-body items-center">
+          <p class="card-title">1 + 0</p>
+          <p class="card-description">Bullet</p>
+        </div>
+      </div>
+      <div class="card card-compact bg-base-300 transition-colors hover:bg-primary">
+        <div class="card-body items-center">
+          <p class="card-title">3 + 0</p>
+          <p class="card-description">Blitz</p>
+        </div>
+      </div>
+      <div class="card card-compact bg-base-300 transition-colors hover:bg-primary">
+        <div class="card-body items-center">
+          <p class="card-title">5 + 0</p>
+          <p class="card-description">Blitz</p>
+        </div>
+      </div>
+      <div class="card card-compact bg-base-300 transition-colors hover:bg-primary">
+        <div class="card-body items-center">
+          <p class="card-title">10 + 0</p>
+          <p class="card-description">Rapid</p>
+        </div>
+      </div>
+      <div class="card card-compact bg-base-300 transition-colors hover:bg-primary">
+        <div class="card-body items-center">
+          <p class="card-title">30 + 0</p>
+          <p class="card-description">Classical</p>
+        </div>
+      </div>
+      <div class="card card-compact bg-base-300 transition-colors hover:bg-primary">
+        <div class="card-body items-center">
+          <p class="card-title">No Time Limit</p>
+        </div>
+      </div>
+    </div>
+    <!-- <div id="home-chat-container"></div> -->
   </div>
 `;
 
@@ -20,5 +57,5 @@ export const component = /*html*/ `
 // }
 
 export function afterInitialize() {
-  renderChatContainer('home-chat-container', SocketEvent.PUBLIC_MESSAGE);
+  // renderChatContainer('home-chat-container', SocketEvent.PUBLIC_MESSAGE);
 }
