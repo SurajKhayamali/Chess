771d4eb7293e6c512000dc9d091d5952c73dcca4
Suraj Khayamali <surajkhayamali.sk@gmail.com>
2024-01-14 22:57:30 +0545

    feat: move pieces by drag and drop functionality

--- frontend-ts/src/components/board/board.component.ts
+++ frontend-ts/src/components/board/board.component.ts
@@ -7,7 +7,7 @@ import {
 } from 'constants/game.constant';
 import { getSquareIndex } from 'scripts/utils';
 
-const renderSquares = (boardDiv: HTMLDivElement) => {
+const renderSquares = (boardDiv: HTMLDivElement, allowMove: boolean) => {
   for (let fileIndex = 0; fileIndex < FILES_LENGTH; fileIndex++) {
     const rank = document.createElement('div');
     rank.classList.add('h-1/8', 'w-full');
@@ -22,6 +22,26 @@ const renderSquares = (boardDiv: HTMLDivElement) => {
         square.classList.add('bg-[#f0d9b5]');
       }
       rank.appendChild(square);
+
+      if (!allowMove) continue;
+      square.ondragover = (e) => {
+        // console.log('drag over', e);
+        e.preventDefault(); // Necessary. Allows us to drop.
+      };
+      square.ondrop = (e) => {
+        console.log('drop', e);
+        e.preventDefault();
+        const data = e.dataTransfer?.getData('text/plain');
+        console.log('data: ', data);
+        if (!data) return;
+        const piece = document.querySelector(`[data-piece="${data}"]`);
+        if (!piece) return;
+        square.appendChild(piece);
+        // const piece = e.dataTransfer?.getData('text/html');
+        // if (!piece) return;
+        // square.innerHTML = piece;
+        // square.appendChild(piece as unknown as Node);
+      };
     }
     boardDiv.appendChild(rank);
   }
@@ -29,7 +49,8 @@ const renderSquares = (boardDiv: HTMLDivElement) => {
 
 const renderPieces = (
   boardDiv: HTMLDivElement,
-  board: ReturnType<Chess['board']>
+  board: ReturnType<Chess['board']>,
+  allowMove = false
 ) => {
   for (let fileIndex = 0; fileIndex < FILES_LENGTH; fileIndex++) {
     for (let rankIndex = 0; rankIndex < RANKS_LENGTH; rankIndex++) {
@@ -49,14 +70,29 @@ const renderPieces = (
         ABBREVIATION_TO_PIECE[type]
       }`;
       // const piece = document.createElement('div');
-      piece.classList.add('h-full', 'w-full');
-      piece.setAttribute('data-piece', square.type);
+      piece.classList.add('h-full', 'w-full', 'cursor-grab');
+      const pieceId = `${color}${type}:${squareId}`;
+      piece.setAttribute('data-piece', `${pieceId}`);
       squareDiv.appendChild(piece);
+
+      if (!allowMove) continue;
+      piece.draggable = true;
+      piece.ondragstart = (e) => {
+        // e.preventDefault();
+        console.log('drag start', e);
+        e.dataTransfer?.setData('text/plain', pieceId);
+        e.dataTransfer?.setData('text/html', piece.outerHTML);
+        // e.dataTransfer?.setDragImage(piece, 0, 0);
+      };
     }
   }
 };
 
-export const renderBoard = (boardContainerId: string, fen: string) => {
+export const renderBoard = (
+  boardContainerId: string,
+  fen: string,
+  allowMove = false
+) => {
   const boardContainer = document.getElementById(boardContainerId);
   if (!boardContainer) return;
 
@@ -68,11 +104,11 @@ export const renderBoard = (boardContainerId: string, fen: string) => {
     'min-w-96',
     'aspect-square'
   );
-  renderSquares(boardDiv);
+  renderSquares(boardDiv, allowMove);
   boardContainer.appendChild(boardDiv);
 
   const chess = new Chess(fen);
   const board = chess.board();
 
-  renderPieces(boardDiv, board);
+  renderPieces(boardDiv, board, allowMove);
 };
