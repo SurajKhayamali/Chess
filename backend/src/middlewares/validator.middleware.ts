9eb8435786c59f114abbde915916671f6a49c0b4
Suraj Khayamali <surajkhayamali.sk@gmail.com>
2024-01-15 18:27:29 +0545

    fix: ```Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client```
    possible cause: next middleware from finally executed even after throwing exception from the above catch block

--- backend/src/middlewares/validator.middleware.ts
+++ backend/src/middlewares/validator.middleware.ts
@@ -8,10 +8,9 @@ export function validateReqQuery(schema: Schema) {
   return (req: Request, _res: Response, next: NextFunction) => {
     try {
       req.query = validateSchema(schema, req.query);
+      next();
     } catch (error) {
       return next(new BadRequestException((error as Error).message));
-    } finally {
-      next();
     }
   };
 }
@@ -20,10 +19,9 @@ export function validateReqBody(schema: Schema) {
   return (req: Request, _res: Response, next: NextFunction) => {
     try {
       req.body = validateSchema(schema, req.body);
+      next();
     } catch (error) {
       return next(new BadRequestException((error as Error).message));
-    } finally {
-      next();
     }
   };
 }
